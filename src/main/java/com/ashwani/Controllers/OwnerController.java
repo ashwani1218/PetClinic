9f943d6600ead255a007651e90f34ffc734bda82
unknown <ashwanipandey1218@gmail.com>
2018-12-30 22:29:04 +0530

    Updated OwnerControllers and index.html of Owners thymeleaf not rendering data

--- src/main/java/com/ashwani/Controllers/OwnerController.java
+++ src/main/java/com/ashwani/Controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package com.ashwani.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ashwani.Services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerSerivce) {
+		super();
+		this.ownerService = ownerSerivce;
+	}
+
 	@RequestMapping({"","/index","/index.html"})
-	public String ListOwners() {
+	public String ListOwners(Model model) {
+		model.addAttribute("owners",ownerService.findAll());
 		return "/Owners/index";
 	}
 }
