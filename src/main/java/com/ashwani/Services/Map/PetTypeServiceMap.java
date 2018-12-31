e02db6926ed8e9fd6d24da2cf439743c97fee65f
unknown <ashwanipandey1218@gmail.com>
2018-12-31 19:46:01 +0530

    Created PetType Map Service closes #9

---
+++ src/main/java/com/ashwani/Services/Map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package com.ashwani.Services.Map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+import com.ashwani.Model.PetType;
+import com.ashwani.Services.PetTypeService;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public Set<PetType> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType Save(PetType Object) {
+		return super.Save(Object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType Object) {
+		super.delete(Object);
+	}
+}
---
+++ src/main/java/com/ashwani/Services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.ashwani.Services;
+
+import com.ashwani.Model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
