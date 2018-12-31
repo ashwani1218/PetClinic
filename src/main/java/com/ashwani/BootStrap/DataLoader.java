4e7d5d36f16d6c6dc8ba2ff2433f0ea42fc4024c
unknown <ashwanipandey1218@gmail.com>
2018-12-31 20:14:19 +0530

    fixed Issue regarding Css not rendering #5

--- src/main/java/com/ashwani/BootStrap/DataLoader.java
+++ src/main/java/com/ashwani/BootStrap/DataLoader.java
@@ -5,8 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.ashwani.Model.Owner;
+import com.ashwani.Model.PetType;
 import com.ashwani.Model.Vet;
 import com.ashwani.Services.OwnerService;
+import com.ashwani.Services.PetTypeService;
 import com.ashwani.Services.VetService;
 
 @Component
@@ -14,16 +16,26 @@ public class DataLoader implements CommandLineRunner {
 
 	private OwnerService ownerService;
 	private VetService vetService;
-
+	private PetTypeService petTypeService; 
+	
 	@Autowired
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService,PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService=petTypeService;
 	}
 	
 	@Override
 	public void run(String... args) throws Exception {
-	
+		
+		PetType dog= new PetType();
+		dog.setName("Dog");
+		PetType savedDogPetType=petTypeService.Save(dog);
+		
+		PetType cat= new PetType();
+		cat.setName("Cat");
+		PetType savedCatPetType=petTypeService.Save(cat);
+		
 		Owner owner1=new Owner();
 		owner1.setFirstName("Ashwani");
 		owner1.setLastName("Pandey");
