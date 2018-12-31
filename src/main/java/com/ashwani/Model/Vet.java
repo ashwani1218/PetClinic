d63ef5c1ed262b5a1db98aad6d2593c068fdfa4d
unknown <ashwanipandey1218@gmail.com>
2018-12-31 18:39:39 +0530

    added Specialty Entity And Added to Vet closes #7

--- src/main/java/com/ashwani/Model/Vet.java
+++ src/main/java/com/ashwani/Model/Vet.java
@@ -1,5 +1,17 @@
 package com.ashwani.Model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+	private Set<Specialty> specialty;
+
+	public Set<Specialty> getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(Set<Specialty> specialty) {
+		this.specialty = specialty;
+	}
+	
 }
