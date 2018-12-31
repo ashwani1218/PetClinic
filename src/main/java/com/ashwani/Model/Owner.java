b6c3697cae197d0203c80ecee2e9320824d48e50
unknown <ashwanipandey1218@gmail.com>
2018-12-31 18:52:21 +0530

    Added contact Info in Owner closes #8

--- src/main/java/com/ashwani/Model/Owner.java
+++ src/main/java/com/ashwani/Model/Owner.java
@@ -4,7 +4,35 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+	private String address;
+	private String city;
+	private String telphone;
 	private Set<Pet> pets;
+
+	
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getTelphone() {
+		return telphone;
+	}
+
+	public void setTelphone(String telphone) {
+		this.telphone = telphone;
+	}
 
 	public Set<Pet> getPets() {
 		return pets;
