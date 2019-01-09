7d4f13d03cfa5a0702ed97f5ad3adfbe56f3fd1f
unknown <ashwanipandey1218@gmail.com>
2019-01-09 18:18:17 +0530

    Added SpecialtiesMapService, closes #11

---
+++ src/main/java/com/ashwani/Services/Map/SpecialtyMapService.java
@@ -0,0 +1,39 @@
+package com.ashwani.Services.Map;
+
+import java.util.Set;
+import org.springframework.stereotype.Service;
+import com.ashwani.Model.Specialty;
+import com.ashwani.Services.SpecialtiesService;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+	@Override
+	public
+	Set<Specialty> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Specialty findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Specialty Save(Specialty Object) {
+		return super.Save(Object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Specialty Object) {
+		super.delete(Object);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/ashwani/Services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.ashwani.Services;
+
+import com.ashwani.Model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
