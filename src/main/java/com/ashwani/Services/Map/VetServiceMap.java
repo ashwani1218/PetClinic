0847e4225a6163c43f5da6837d357cf4de12b631
unknown <ashwanipandey1218@gmail.com>
2019-01-11 16:39:28 +0530

    Created specialties and added to the vet on startup closes #11

--- src/main/java/com/ashwani/Services/Map/VetServiceMap.java
+++ src/main/java/com/ashwani/Services/Map/VetServiceMap.java
@@ -4,11 +4,20 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import com.ashwani.Model.Specialty;
 import com.ashwani.Model.Vet;
+import com.ashwani.Services.SpecialtyService;
 import com.ashwani.Services.VetService;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService {
+
+	private SpecialtyService specialtyService;
+	
+	public VetServiceMap(SpecialtyService specialtyService) {
+		super();
+		this.specialtyService = specialtyService;
+	}
 
 	@Override
 	public Set<Vet> findAll(){
@@ -22,7 +31,15 @@ public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetSe
 	
 	@Override
 	public Vet Save(Vet object) {
-		// TODO Auto-generated method stub
+		if(object.getSpecialty().size()>0) {
+			object.getSpecialty().forEach(specialty->{
+				if(specialty.getId()==null) {
+					Specialty savedSpecialty= specialtyService.Save(specialty);
+					specialty.setId(savedSpecialty.getId());
+					
+				}
+			});
+		}
 		return super.Save(object);
 	}
 
