e88fae85a7a7ee90d802e910d6384ecbf8164cc4
unknown <ashwanipandey1218@gmail.com>
2019-01-15 23:33:33 +0530

    Converted Vets to JPA entity, closes #17

--- src/main/java/com/ashwani/Model/Specialty.java
+++ src/main/java/com/ashwani/Model/Specialty.java
@@ -1,7 +1,14 @@
 package com.ashwani.Model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="specialty")
 public class Specialty extends BaseEntity {
-	
+
+	@Column(name="description")
 	private String description;
 
 	public String getDescription() {
--- src/main/java/com/ashwani/Model/Vet.java
+++ src/main/java/com/ashwani/Model/Vet.java
@@ -2,9 +2,20 @@ package com.ashwani.Model;
 
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="vets")
 public class Vet extends Person {
 
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(name="vet_specialties", joinColumns = @JoinColumn(name="vet_id"),
+	inverseJoinColumns = @JoinColumn(name="specialty_id"))
 	private Set<Specialty> specialty=new HashSet<>();
 
 	public Set<Specialty> getSpecialty() {
