b6f09fdd27d32a439bc327df0aa12b30eecf1344
unknown <ashwanipandey1218@gmail.com>
2019-01-15 23:01:16 +0530

    Implemented Base Entity, closes #15

--- src/main/java/com/ashwani/Model/BaseEntity.java
+++ src/main/java/com/ashwani/Model/BaseEntity.java
@@ -2,8 +2,16 @@ package com.ashwani.Model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable{
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
