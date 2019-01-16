2369dd3bb5574242d19a3a950f4e0063280b5fe2
unknown <ashwanipandey1218@gmail.com>
2019-01-17 00:33:38 +0530

    Created Visit Map Service ,closes #26

---
+++ src/main/java/com/ashwani/Services/Map/VisitMapService.java
@@ -0,0 +1,44 @@
+package com.ashwani.Services.Map;
+
+import java.util.Set;
+
+import com.ashwani.Model.Visit;
+import com.ashwani.Services.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+	@Override
+	public Set<Visit> findAll() { 
+		return super.findAll();
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Visit Save(Visit visit) {
+		
+		if(visit.getPet()==null || visit.getPet().getOwner()==null || visit.getPet().getId()==null
+				|| visit.getPet().getOwner().getId()==null) {
+			throw new RuntimeException("Invalid Visit");
+			
+		}
+		
+		return super.Save(visit);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Visit Object) {
+		super.delete(Object);
+	}
+
+	
+	
+}
---
+++ src/main/java/com/ashwani/Services/VisitService.java
@@ -0,0 +1,7 @@
+package com.ashwani.Services;
+
+import com.ashwani.Model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+
+}
